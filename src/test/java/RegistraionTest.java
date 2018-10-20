c630e5b07d89d7f7003cb65fe8c895d76f5c4a40
Cormac Buckley <cormac.buckley@sap.com>
2018-10-20 17:21:16 +0100

    Tidied up test class

--- src/test/java/RegistraionTest.java
+++ src/test/java/RegistraionTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import com.mycompany.studentregistraionsystem.CourseProgramme;
 import com.mycompany.studentregistraionsystem.Module;
@@ -22,87 +17,67 @@ import org.junit.Test;
  * @author I342042
  */
 public class RegistraionTest {
-    
+
     private Student student;
     private Module module;
     private CourseProgramme cp;
-    
-    
+
     ArrayList<CourseProgramme> Courses = new ArrayList<CourseProgramme>();
     ArrayList<Student> Students = new ArrayList<Student>();
     ArrayList<Module> Modules = new ArrayList<Module>();
-    
-    public RegistraionTest() {
-    }
-    
-//    @BeforeClass
-//    public static void setUpClass() {
-//    }
-//    
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//    
+
     @Before
     public void setUp() {
-         
-        
-        cp = new CourseProgramme("BCT", module, new DateTime("2012-08-16"),new DateTime("2013-08-16") );
+
+        cp = new CourseProgramme("BCT", module, new DateTime("2012-08-16"), new DateTime("2013-08-16"));
         student = new Student("John Smith", 35, "01/01/2000", "1234", cp);
         module = new Module("CT414", student, cp);
-        cp = new CourseProgramme("BCT", module, new DateTime("2012-08-16"),new DateTime("2013-08-16") );
-       
+        cp = new CourseProgramme("BCT", module, new DateTime("2012-08-16"), new DateTime("2013-08-16"));
+
     }
-//    
-//    @After
-//    public void tearDown() {
-//    }
-//
+
     @Test
-     public void Student_Tests() {
+    public void Student_Tests() {
+        CourseProgramme[] sendCP = new CourseProgramme[10];
+        sendCP[0] = cp;
+
         student.setName("John Smith");
         student.setAge(35);
         student.setDOB("01/01/2000");
-        
-        
-        CourseProgramme[] sendCP = new CourseProgramme[10];
-            sendCP[0] = cp;
         student.setCourses(sendCP);
         student.setID("123456");
-        
-        
+
         assertEquals(student.getDOB(), "01/01/2000");
         assertEquals(student.getUsername(), "John Smith35");
         assertEquals(student.getCourses()[0].getName(), "BCT");
         assertEquals(student.getID(), "123456");
-        
-     }
-     
-     @Test
-     public void Module_Tests() {
+
+    }
+
+    @Test
+    public void Module_Tests() {
         Student[] sendStud = new Student[10];
         sendStud[0] = student;
-        
-            CourseProgramme[] sendCP = new CourseProgramme[10];
-            sendCP[0] = cp;
-        
+
+        CourseProgramme[] sendCP = new CourseProgramme[10];
+        sendCP[0] = cp;
+
         module.setStudents(sendStud);
         module.setName("CT414");
         module.setCourses(sendCP);
-        
-        
-        assertEquals(module.getStudents()[0].getName(), "John Smith"); 
+
+        assertEquals(module.getStudents()[0].getName(), "John Smith");
         assertEquals(module.getName(), "CT414");
         assertEquals(module.getCourses()[0].getName(), "BCT");
-        
-     }
-     
-     @Test
-     public void CP_Tests() {
-         Module[] send = new Module[10];
-         send[0] = module;
+
+    }
+
+    @Test
+    public void CP_Tests() {
+        Module[] send = new Module[10];
+        send[0] = module;
         cp.setModules(send);
-        assertEquals(cp.getModules()[0].getName(), "CT414");  
-        
-     }
+        assertEquals(cp.getModules()[0].getName(), "CT414");
+
+    }
 }
